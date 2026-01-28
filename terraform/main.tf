1691b15dc377c0eceed16829cb913eb8583c6add
olawale lateef <olawalelateef9@gmail.com>
2026-01-28 08:06:42 -1200

    new pipelines

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  backend "s3" {
+    bucket  = "olawale-s3-devops-bucket"
+    key     = "envs/dev/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }  
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+# 1. Nginx Node
+resource "aws_instance" "nginx-node" {
+  ami                    = "ami-03ea746da1a2e36e7"
+  instance_type          = "t2.small"
+  subnet_id              = "subnet-03e8a88d085ee2c50"
+  vpc_security_group_ids = ["sg-06aab2abe1f311dbc"]
+  key_name               = "jenkinskp"
+
+  tags = {
+    Name = "Nginx_node"
+  }
+}
+
+# 2. Java Node
+resource "aws_instance" "java-node" {
+  ami                    = "ami-03ea746da1a2e36e7"
+  instance_type          = "t2.small"
+  subnet_id              = "subnet-03e8a88d085ee2c50"
+  vpc_security_group_ids = ["sg-06aab2abe1f311dbc"]
+  key_name               = "jenkinskp"
+
+  tags = {
+    Name = "Java_Node"
+  }
+}
+
+
